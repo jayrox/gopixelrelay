3b98ea4d418ea502f5fa5ac7410ec16c0bcdce3e
jay <jayrox@gmail.com>
2014-02-17 11:59:20 -0500

    support for file tagging

--- db/db.go
+++ db/db.go
@@ -35,10 +35,12 @@ func Logger(db *gorm.DB, enable bool) {
 
 func MigrateDB(db *gorm.DB) {
 	fmt.Println("updating tables")
-	db.AutoMigrate(models.User{})
 	db.AutoMigrate(models.Album{})
 	db.AutoMigrate(models.Image{})
+	db.AutoMigrate(models.ImageTag{})
+	db.AutoMigrate(models.Tag{})
 	db.AutoMigrate(models.Uploader{})
+	db.AutoMigrate(models.User{})
 	fmt.Println("completed updating tables")
 }
 
@@ -73,6 +75,44 @@ func AddImage(db *gorm.DB, image models.Image) models.Image {
 	return image
 }
 
+// Tag Image
+func TagImage(db *gorm.DB, tag string, name string) models.ImageTag {
+	// Get tag id or create tag
+	var mTag models.Tag
+	db.Where(models.Tag{Name: tag}).FirstOrCreate(&mTag)
+	//db.FirstOrCreate(&mTag, models.Tag{Name: tag})
+
+	// Get Image Id
+	var image models.Image
+	db.First(&image, "name = ?", name)
+
+	// Add tag to image
+	imagetag := models.ImageTag{ImgId: image.Id, TagId: mTag.Id}
+
+	// Save tag
+	db.NewRecord(&imagetag)
+	db.Save(&imagetag)
+	db.NewRecord(&imagetag)
+	
+	return imagetag
+}
+
+// Get Images with Tag
+func GetImagesWithTag(db *gorm.DB, tag string) []models.TaggedImage {
+	var images []models.TaggedImage
+	//query := db.Exec("SELECT images.id as image_id, images.name as name, tags.name as tag FROM images LEFT JOIN image_tags ON (image_tags.img_id = images.id) LEFT JOIN tags ON (image_tags.tag_id = tags.id AND image_tags.img_id = images.id) WHERE tags.name = ? ORDER BY images.id ASC", tag).Scan(&images)
+	db.Table("images").Select("images.id as image_id, images.name as name, tags.name as tag").Joins("LEFT JOIN image_tags ON (image_tags.img_id = images.id) LEFT JOIN tags ON (image_tags.tag_id = tags.id AND image_tags.img_id = images.id)").Where("tags.name = ?", tag).Order("images.id ASC").Scan(&images)
+	fmt.Println(images)
+	return images
+}
+
+// Get all tags
+func GetAllTags(db *gorm.DB) []models.Tag {
+	var tags []models.Tag
+	db.Where("name != ''").Find(&tags)
+	return tags
+}
+
 // Add upload to uploader history/logging
 func AddUpload(db *gorm.DB, upload models.Uploader) {
 	db.NewRecord(&upload)
@@ -91,6 +131,8 @@ func DropTables(db *gorm.DB) {
 	fmt.Println("dropping tables")
 	db.DropTable(models.Album{})
 	db.DropTable(models.Image{})
+	db.DropTable(models.ImageTag{})
+	db.DropTable(models.Tag{})
 	db.DropTable(models.Uploader{})
 	db.DropTable(models.User{})
 }
@@ -99,6 +141,8 @@ func AddTables(db *gorm.DB) {
 	fmt.Println("adding tables")
 	db.CreateTable(models.Album{})
 	db.CreateTable(models.Image{})
+	db.CreateTable(models.ImageTag{})
+	db.CreateTable(models.Tag{})
 	db.CreateTable(models.Uploader{})
 	db.CreateTable(models.User{})
 }
