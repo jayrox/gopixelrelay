ff32c8ca192e0d175c02ab74b9c6391b62309506
jay <jayrox@gmail.com>
2014-02-17 11:56:00 -0500

    start support for sessions

--- controllers/album.go
+++ controllers/album.go
@@ -8,15 +8,19 @@ import (
 	"pixelrelay/db"
 )
 
-func Album(args martini.Params, r render.Render) {
+func Album(args martini.Params, session sessions.Session, r render.Render) {
 	album := args["name"]
 	d := db.InitDB()
 	images := db.GetAllAlbumImages(&d, album)
+	session.Set("album", album)
 
 	var imageLinks []ImageLink
 	for _, f := range images {
 		imageLinks = append(imageLinks, ImageLink{Title: f.Name, FileName: f.Name})
 	}
+	
+	v := session.Get("album")
+	fmt.Printf("sessions: %s\n", v)
 
 	r.HTML(200, "image_link", imageLinks)
 }
