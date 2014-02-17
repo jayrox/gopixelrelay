7f0427b53f74175c280a7b9230ff759ac6738cdd
jay <jayrox@gmail.com>
2014-02-17 11:59:46 -0500

    go fmt

--- models/user.go
+++ models/user.go
@@ -4,6 +4,6 @@ type User struct {
 	Id        int64
 	Name      string `sql:"type:varchar(255);"`
 	Password  string `sql:"type:varchar(255);"`
-	Salt  string `sql:"type:varchar(255);"`
+	Salt      string `sql:"type:varchar(255);"`
 	Timestamp int64
 }
