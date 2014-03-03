58db0fc78d7db6ad59795315c418c9d790380283
jay <jayrox@gmail.com>
2014-03-03 16:07:55 -0500

    Page struct to hold all of the data that is common on each page template

--- models/pagevars.go
+++ models/pagevars.go
@@ -1,5 +1,41 @@
 package models
 
-func PageVars() (pageVars map[string]interface{}) {
-	return
+import (
+	"strings"
+
+	"pixelrelay/utils"
+)
+
+type Page struct {
+	Url       string
+	User      User
+	SiteTitle string
+	Title     string
+}
+
+func (p *Page) SetUser(user User) {
+	p.User = user
+}
+
+func (p *Page) SetUrl(url string) {
+	p.Url = url
+}
+
+func (p *Page) SetTitle(title string) {
+	p.Title = strings.Join([]string{utils.AppCfg.Title(), title}, " :: ")
+}
+
+func (p *Page) SetSiteTitle(title string) {
+	p.SiteTitle = title
+}
+
+func (p *Page) SetDefaults() {
+	p.SiteTitle = utils.AppCfg.Title()
+	p.Title = utils.AppCfg.Title()
+	p.Url = utils.AppCfg.Url()
+}
+
+func InitPage(p *Page) *Page {
+	p.SetDefaults()
+	return p
 }
