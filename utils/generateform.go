5df4c8256d7c27cdb31ba5383e5bebddd7dd367c
jay <jayrox@gmail.com>
2014-03-04 08:06:18 -0500

    support for nested structs in form generation

--- utils/generateform.go
+++ utils/generateform.go
@@ -11,8 +11,7 @@ import (
 func GenerateForm(fields interface{}, action string, method string, errs map[string]string) template.HTML {
 	var form string
 	formname := strings.Split(reflect.TypeOf(fields).String(), ".")[1]
-
-	log.Println(errs)
+	log.Println("formname: ", formname)
 
 	form += fmt.Sprintf("\t<form name=\"%s\" action=\"%s\" method=\"%s\">\n", formname, action, strings.ToUpper(method))
 
@@ -22,75 +21,89 @@ func GenerateForm(fields interface{}, action string, method string, errs map[str
 
 	val := reflect.ValueOf(fields).Elem()
 
+	form += generateInputs(val, errs)
+	form += "    </form>\n"
+
+	return template.HTML(form)
+}
+
+func generateInputs(val reflect.Value, errs map[string]string) (inputs string) {
 	for i := 0; i < val.NumField(); i++ {
 		valueField := val.Field(i).String()
 		typeField := val.Type().Field(i)
 		tag := typeField.Tag
 
-		// Ignore "-" form fields
-		if tag.Get("form") == "-" {
+		if tag.Get("form") == "inherit" {
+			inputs += generateInputs(val.Field(i), errs)
 			continue
 		}
 
-		var extras string
-		var label string
+		inputs += generateInput(tag, valueField)
 
-		fType := "text"
-		fAttrs := strings.Split(tag.Get("attr"), ";")
-		for _, element := range fAttrs {
-			fAttr := strings.Split(element, ":")
-			ele := strings.ToLower(fAttr[0])
-
-			val := ""
-			if len(fAttr) == 2 {
-				val = fAttr[1]
-			}
-			if valueField != "" && val == "input" {
-				val = valueField
-			}
-			if valueField == "" && val == "input" {
-				val = ""
-			}
-
-			switch ele {
-			case "alt":
-				extras += fmt.Sprintf(" alt=\"%s\"", val)
-			case "autofocus":
-				extras += " autofocus"
-			case "checked":
-				extras += " checked"
-			case "class":
-				extras += fmt.Sprintf(" class=\"%s\"", val)
-			case "label":
-				label = fmt.Sprintf("\t<label for=\"%s\">%s</label>\n", tag.Get("form"), val)
-			case "maxlength":
-				extras += fmt.Sprintf(" maxlength=\"%s\"", val)
-			case "min":
-				extras += fmt.Sprintf(" min=\"%s\"", val)
-			case "placeholder":
-				extras += fmt.Sprintf(" placeholder=\"%s\"", val)
-			case "readonly":
-				extras += " readonly"
-			case "required":
-				extras += " required"
-			case "type":
-				fType = val
-			case "value":
-				extras += fmt.Sprintf(" value=\"%s\"", val)
-			}
-		}
-
-		if label != "" {
-			form += label
-		}
-
-		form += fmt.Sprintf("\t<input type=\"%s\" name=\"%s\" id=\"%s\"%s>\n", fType, tag.Get("form"), tag.Get("form"), extras)
 		if errs[tag.Get("form")] != "" {
-			form += fmt.Sprintf("\t<span class=\"flash\">%s</span>\n", errs[tag.Get("form")])
+			inputs += fmt.Sprintf("\t<span class=\"flash\">%s</span>\n", errs[tag.Get("form")])
+		}
+	}
+	return
+}
+
+func generateInput(tag reflect.StructTag, valueField string) (fInput string) {
+	var extras, label string
+
+	// Ignore "-" form fields
+	if tag.Get("form") == "-" || tag.Get("form") == "" {
+		return
+	}
+
+	fType := "text"
+	fAttrs := strings.Split(tag.Get("attr"), ";")
+	for _, element := range fAttrs {
+		fAttr := strings.Split(element, ":")
+		ele := strings.ToLower(fAttr[0])
+
+		val := ""
+		if len(fAttr) == 2 {
+			val = fAttr[1]
+		}
+		if valueField != "" && val == "input" {
+			val = valueField
+		}
+		if valueField == "" && val == "input" {
+			val = ""
+		}
+
+		switch ele {
+		case "alt":
+			extras += fmt.Sprintf(" alt=\"%s\"", val)
+		case "autofocus":
+			extras += " autofocus"
+		case "checked":
+			extras += " checked"
+		case "class":
+			extras += fmt.Sprintf(" class=\"%s\"", val)
+		case "label":
+			label = fmt.Sprintf("\t<label for=\"%s\">%s</label>\n", tag.Get("form"), val)
+		case "maxlength":
+			extras += fmt.Sprintf(" maxlength=\"%s\"", val)
+		case "min":
+			extras += fmt.Sprintf(" min=\"%s\"", val)
+		case "placeholder":
+			extras += fmt.Sprintf(" placeholder=\"%s\"", val)
+		case "readonly":
+			extras += " readonly"
+		case "required":
+			extras += " required"
+		case "type":
+			fType = val
+		case "value":
+			extras += fmt.Sprintf(" value=\"%s\"", val)
 		}
 	}
 
-	form += "    </form>\n"
+	if label != "" {
+		fInput += label
+	}
 
-	return template.HTML(form)
+	fInput += fmt.Sprintf("\t<input type=\"%s\" name=\"%s\" id=\"%s\"%s>\n", fType, tag.Get("form"), tag.Get("form"), extras)
+	return
 }
