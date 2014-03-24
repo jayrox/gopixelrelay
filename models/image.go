1617599a4cc66814fd08b6d9d99aae783b6eb157
Jason Boehm <jayrox@gmail.com>
2014-03-24 11:49:36 -0400

    add support for hashid to remove db ids and add album id to support album name changes

--- models/image.go
+++ models/image.go
@@ -2,9 +2,11 @@ package models
 
 type Image struct {
 	Id          int64
+	HashId      string
 	User        int64
 	Name        string `sql:"not null;unique;type:varchar(255);"`
 	Album       string `sql:"type:varchar(255);"`
+	AlbumId     int64
 	Title       string `sql:"type:varchar(255);"`
 	Description string `sql:"type:varchar(1024);"`
 	Timestamp   int64
